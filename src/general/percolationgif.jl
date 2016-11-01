77acf920418c62c5eaf6e9814a4960dbc4b5895b
goropikari <goropikari@users.noreply.github.com>
2016-11-01 19:45:50 +0900

    minor change

--- src/general/percolationgif.jl
+++ src/general/percolationgif.jl
@@ -1,22 +1,23 @@
-function percolationgif(Lattice::SimpleLattice; output_dir=tempdir()*"/Percolation_"*randstring(), color="brg_r", colorbar=true, fps=4, filename="anime.gif")
+function percolationgif(Lattice::SimpleLattice; output_dir="./", color="brg_r", colorbar=true, fps=4, filename="anime.gif")
+    output_tempolary_png=tempdir()*"/Percolation_"*randstring()
     (row, column) = (Lattice.N, Lattice.M)
     if 2 ∈ Lattice.lattice[row, :]; hit = 1; else; hit = 0; end
-    PercolationPlot(Lattice, hit, 0, output_dir, color, colorbar, fps)
+    PercolationPlot(Lattice, hit, 0, output_tempolary_png, color, colorbar)
     PyPlot.clf()
     previous_lattice = ones(Int64, row, column)
     
     PyPlot.ioff()
     indx = 1
-    while Lattice.lattice != exconfig
-        exconfig = Lattice.lattice[:,:]
+    while Lattice.lattice != previous_lattice
+        previous_lattice = Lattice.lattice[:,:]
         Lattice.lattice = checkallsite(Lattice)
         if 2 ∈ Lattice.lattice[row, :]; hit = 1; else; hit = 0; end
-        PercolationPlot(Lattice, hit, indx, output_dir, color, colorbar, fps)
+        PercolationPlot(Lattice, hit, indx, output_tempolary_png, color, colorbar)
         PyPlot.clf()
         indx += 1
     end
-    run(`convert -delay $fps $output_dir/*.png $filename`)
-    rm(output_dir, force=true, recursive=true)
+    run(`convert -delay $fps $(output_tempolary_png)/*.png $(output_dir)/$filename`)
+    rm(output_tempolary_png, force=true, recursive=true)
     close()
     
     gc()
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,17 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 2
+site = simplenn(10,10,0.5)
+percolation(site)
+percolation(site, water=false)
+percolation(site, water=false, colorbar=false)
+percolation(site, water=false, color="hot")
+percolation(site, water=false, fig=false)
+
+site = simplennn(10,10,0.5)
+percolation(site)
+percolation(site, water=false)
+percolation(site, water=false, colorbar=false)
+percolation(site, water=false, color="hot")
+percolation(site, water=false, fig=false)
+
