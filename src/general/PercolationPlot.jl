c9319750fca06030c833892f24b256e1a900473e
goropikari <goropikari@users.noreply.github.com>
2016-11-12 16:51:20 +0900

    trinn cluster

--- src/general/PercolationPlot.jl
+++ src/general/PercolationPlot.jl
@@ -47,13 +47,14 @@ function percolationplot(Lattice::TriangularLattice; waterplot=true)
     if 2 ∈ Lattice.lattice[Lattice.N, :]; hit = 1; else; hit = 0; end
     
     ywater, xwater = findn( flipdim( (Lattice.lattice .== 2) * 1 , 1) )
-	yempty, xempty = findn( flipdim( (Lattice.lattice .== 1) * 1 , 1) )
+#	yempty, xempty = findn( flipdim( (Lattice.lattice .== 1) * 1 , 1) )
 	yblock, xblock = findn( flipdim( (Lattice.lattice .== 0) * 1 , 1) )
 	
 
     PyPlot.figure()
-    if waterplot; PyPlot.plot(xwater, ywater, "b."); else; PyPlot.plot(xwater, ywater, "o", color="white"); end
-    PyPlot.plot(xempty, yempty, ".", color="white")
+#    if waterplot; PyPlot.plot(xwater, ywater, "b."); else; PyPlot.plot(xwater, ywater, "o", color="white"); end
+    if waterplot; PyPlot.plot(xwater, ywater, "b."); end
+#    PyPlot.plot(xempty, yempty, ".", color="white")
     PyPlot.plot(xblock, yblock, ".", color="black")
 	PyPlot.axis("equal")
 #	PyPlot.axis("off")
