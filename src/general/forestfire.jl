8b41932348ef63d00112b5a1c0d51316646905a5
goropikari <goropikari@users.noreply.github.com>
2016-11-16 19:23:33 +0900

    add forest fire

---
+++ src/general/forestfire.jl
@@ -0,0 +1,151 @@
+#using PyPlot, PyCall
+#@pyimport matplotlib.patches as patch
+
+#0: empty
+#1: green
+#2: fire
+type forest
+    N::Int
+    p::Float64
+    lattice::Matrix{Int}
+    
+    function forest(N, p)
+        lattice = ( rand(N, N) .< p ) * 1
+        for i in 1:N
+            if lattice[i] == 1
+                lattice[i] = 2
+            end
+        end
+        
+        new(N, p, lattice)
+    end
+end
+
+function forestfire(Lattice::forest)
+    lifetime = 0
+    previous_lattice = Lattice.lattice[:,:]
+    
+    checkallsite(Lattice)
+    while previous_lattice != Lattice.lattice
+        previous_lattice = Lattice.lattice[:,:]
+        checkallsite(Lattice)
+        lifetime += 1
+    end
+    
+    return lifetime
+end
+
+# Introduction to Percolation Theory revised 2nd ed, Stauffer, p.6
+#plist = collect(0:0.02:1)
+#time = zeros(Int, length(plist))
+#trial = 50
+#len = length(plist)
+#@time for i in 1:len, j in 1:trial
+#	site = forest(100, plist[i])
+#	time[i] += forestfire(site)
+#end
+
+#plot(plist, time/trial)
+#show()
+
+
+
+function forestfiregif(Lattice::forest)
+    close()
+    lifetime = 0
+    previous_lattice = Lattice.lattice[:,:]
+    
+    # plot initial configuration
+    forestplot(Lattice, lifetime)
+    
+    checkallsite(Lattice)
+    while previous_lattice != Lattice.lattice
+        lifetime += 1
+        forestplot(Lattice, lifetime)
+        previous_lattice = Lattice.lattice[:,:]
+        checkallsite(Lattice)
+    end
+    
+    return lifetime
+end
+
+
+
+
+
+
+#function checksitenn(i::Int, j::Int, lattice::Array{Int})
+#	(row,  column) = size(lattice)
+#	if j < column && lattice[i, j+1] == 1; lattice[i, j+1] = 2; end
+#	if 1 < j && lattice[i, j-1] == 1; lattice[i, j-1] = 2; end
+#	if i < row && lattice[i+1, j] == 1; lattice[i+1, j] = 2; end
+#	if 1 < i && lattice[i-1, j] == 1; lattice[i-1, j] = 2; end
+
+#	return lattice
+#end
+
+# square lattice nearest neighbor
+function checksite(i::Int, j::Int, Lattice::forest)
+    checksitenn(i::Int, j::Int, Lattice.lattice::Array{Int})
+end
+
+function checkallsite(Lattice::forest)
+    (row, column) = size(Lattice.lattice)
+    for i in 1:row, j in 1:column
+        if Lattice.lattice[j,i] == 2
+            Lattice.lattice = checksite(j, i, Lattice)
+        end
+    end
+end
+
+
+##################
+# plot 関連
+##################
+function forestplot(Lattice::forest, lifetime)
+    yfire, xfire = findn( (Lattice.lattice .== 2) * 1 )
+    ytree, xtree = findn( (Lattice.lattice .== 1) * 1 )
+    yempty, xempty = findn( (Lattice.lattice .== 0) * 1 )
+    
+    PyPlot.ioff()
+    #Grid(1, Lattice.N, 1, Lattice.N)
+    for i in 1:length(xfire)
+        circle(xfire[i], yfire[i], 0.25, "r")
+    end
+    for i in 1:length(xtree)
+        circle(xtree[i], ytree[i], 0.25, "g")
+    end
+    for i in 1:length(xempty)
+        circle(xempty[i], yempty[i], 0.25, "k", FILL=false)
+    end
+    
+    
+    PyPlot.title(@sprintf("%05d", lifetime))
+    PyPlot.axis("equal")
+    PyPlot.axis("off")
+    PyPlot.axis([-1, Lattice.N+1, -1, Lattice.N+1])
+    PyPlot.savefig(@sprintf("%05d.png", lifetime))
+    PyPlot.clf()
+
+end
+
+# 丸を書く
+function circle(x, y, r, COLOR; FILL=true)
+    maru = patch.Circle((x,y), radius=r, color=COLOR, fill=FILL);
+    PyPlot.gca()[:add_patch](maru);
+end
+
+# 格子を書く
+function Grid(a, b, c, d)
+    hx = [a, b]
+    hy = [[i, i] for i in c:d]
+    for i in 1:length(collect(c:d))
+        PyPlot.plot(hx, hy[i], "k")
+    end
+
+    vx = [[i, i] for i in a:b]
+    vy = [c, d]
+    for i in 1:length(collect(a:b))
+        PyPlot.plot(vx[i], vy, "k")
+    end
+end
