ea8966df360b3571600c7f5de0c2bc7e19b33f84
goropikari <goropikari@users.noreply.github.com>
2017-12-26 12:12:54 +0900

    update

--- src/Percolation.jl
+++ src/Percolation.jl
@@ -1,26 +1,21 @@
 module Percolation
 
 # type
-export Squarenn, Squarennn
-export Forest
-export Tri
-export Honeycomb
-export Kagome
-export Simplenn
+export Squarenn, Squarennn, Forest, Tri, Honeycomb, Kagome, Simplenn
 
 # function
 #export percolation, percolationplot, percolationgif
-export cluster, clusterplot, clusterplotsize
-export forest, forestgif
+export cluster!, clusterplot, clusterplotsize
+export forestfire!, forestplot, forestgif!
 export HK!
     
-    import PyPlot
-    using LaTeXStrings, StatsBase, PyCall
-    @pyimport matplotlib.patches as patch
+import PyPlot
+using LaTeXStrings, StatsBase, PyCall
+@pyimport matplotlib.patches as patch
 
-    include("latticetype.jl")
-    include("cluster.jl")
-    include("forestfire.jl")
-    include("HoshenKopelmanAlgorithm.jl")    
+include("latticetype.jl")
+include("cluster.jl")
+include("forestfire.jl")
+include("HoshenKopelmanAlgorithm.jl")    
 
 end
